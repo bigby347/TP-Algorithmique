2a173ea12d7290df947749df14c85e3e112e895c
bigby347 <alexandreputzu@gmail.com>
2018-10-03 09:18:19 +0200

    ajout stack et vector

---
+++ TP1/src/main/java/Stack.java
@@ -0,0 +1,32 @@
+public class Stack {
+
+    Vector vector;
+
+    public Stack() {
+        vector = new Vector();
+    }
+
+    public void push(int value) {
+        vector.add(value);
+    }
+
+    public int peek() {
+        return vector.get(vector.size() - 1);
+    }
+
+    public int pop(){
+        int size = vector.size();
+        int value = vector.get(size - 1);
+        vector.resize(size - 1);
+
+        return value;
+    }
+
+    public int size(){
+        return vector.size();
+    }
+
+    public boolean isEmpty(){
+        return vector.isEmpty();
+    }
+}
